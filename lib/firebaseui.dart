8892c840cc8fb957a193e009b70ed1afa19aef19
zgx <slys12345>
2017-06-06 18:19:00 +0800

    commit firebaseui.dart

--- lib/firebaseui.dart
+++ lib/firebaseui.dart
@@ -9,8 +9,8 @@ class Firebaseui {
   static Future<String> get platformVersion =>
       _channel.invokeMethod('getPlatformVersion');
 
-  static Future<bool> get signinstatus =>
-      _channel.invokeMethod('getSignStatus');
+  static Future<FirebaseUser> get currentUser =>
+      _channel.invokeMethod('getCurrentUser');
 
   static Future<String> get signin =>
       _channel.invokeMethod('signIn');
@@ -18,4 +18,53 @@ class Firebaseui {
   static Future<bool> get signout =>
       _channel.invokeMethod('signOut');
 
+}
+
+/// Represents user data returned from an identity provider.
+class UserInfo {
+  final Map<String, dynamic> _data;
+  UserInfo._(this._data);
+
+  /// The provider identifier.
+  String get providerId => _data['providerId'];
+
+  /// The provider’s user ID for the user.
+  String get uid => _data['uid'];
+
+  /// The name of the user.
+  String get displayName => _data['displayName'];
+
+  /// The URL of the user’s profile photo.
+  String get photoUrl => _data['photoUrl'];
+
+  /// The user’s email address.
+  String get email => _data['email'];
+
+  @override
+  String toString() {
+    return '$runtimeType($_data)';
+  }
+}
+
+/// Represents a user.
+class FirebaseUser extends UserInfo {
+  final Map<String, dynamic> _data;
+  final List<UserInfo> providerData;
+  FirebaseUser._(this._data)
+      : providerData = (_data['providerData'] as List<Map<String, dynamic>>)
+      .map((Map<String, dynamic> info) => new UserInfo._(info)).toList(),
+        super._(_data);
+
+  // Returns true if the user is anonymous; that is, the user account was
+  // created with signInAnonymously() and has not been linked to another
+  // account.
+  bool get isAnonymous => _data['isAnonymous'];
+
+  /// Returns true if the user's email is verified.
+  bool get isEmailVerified => _data['isEmailVerified'];
+
+  @override
+  String toString() {
+    return '$runtimeType($_data)';
+  }
 }
